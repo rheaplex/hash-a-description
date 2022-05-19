6d9695907d009c35bb1caee92abfea7a378dfe0e
Rhea Myers <rhea@myers.studio>
2022-05-19 15:51:31 -0700

    Make the search multi-threaded to speed it up / scale it.

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,47 @@
 
 use regex::Regex;
 use sha256::digest;
+use std::sync::Arc;
+use std::thread;
 
 // We generate lots of very similar prefixes to have the best chance of
 // producing a hash that matches when we prefix a statement with them.
 
-const HASH: [&str; 6] = ["hash", "cryptographic hash", "sha256", "SHA256", "sha256 hash", "SHA256 hash"];
-const HASH_ACTION: [&str; 5] = ["hashing", "cryptographic hashing", "cryptographically hashing", "sha256 hashing", "SHA256 hashing"];
+const HASH: [&str; 6] = [
+    "hash",
+    "cryptographic hash",
+    "sha256",
+    "SHA256",
+    "sha256 hash",
+    "SHA256 hash",
+];
+const HASH_ACTION: [&str; 5] = [
+    "hashing",
+    "cryptographic hashing",
+    "cryptographically hashing",
+    "sha256 hashing",
+    "SHA256 hashing",
+];
 const SUBJECT: [&str; 4] = ["this", "this text", "this sentence", "this statement"];
 const RELATION: [&str; 3] = ["contains", "has", "includes"];
 const RESULT: [&str; 4] = ["results in", "produces", "creates", "outputs"];
-const HASH_RESULT: [&str; 5] = ["a string", "a digest", "a hexadecimal string", "a message digest", "a hexadecimal message digest"];
+const HASH_RESULT: [&str; 5] = [
+    "a string",
+    "a digest",
+    "a hexadecimal string",
+    "a message digest",
+    "a hexadecimal message digest",
+];
 
 //const NUMBERS: [&str; 16] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "A", "B", "C", "D", "E", "F"];
 //const NUMBERS_PLURAL: [&str; 16] = ["zeros", "ones", "twos", "threes", "fours", "fives", "sixes", "sevens", "eights", "nines", "As", "Bs", "Cs", "Ds", "Es", "Fs"];
 
-
-fn initial_cap (text: &str) -> String {
+fn initial_cap(text: &str) -> String {
     let result = String::from(&text[..1]).to_uppercase();
     return result + &text[1..];
 }
 
-fn gen_prefixes () -> Vec<String> {
+fn gen_prefixes() -> Vec<String> {
     let mut prefixes = Vec::new();
     // e.g. "the sha256 hash of this text includes"
     for hash in HASH {
@@ -53,7 +73,10 @@ fn gen_prefixes () -> Vec<String> {
             for result in RESULT {
                 for hash_result in HASH_RESULT {
                     for relation in RELATION {
-                        let prefix = format!("{} {} {} {} that {}", hash_action, subject, result, hash_result, relation);
+                        let prefix = format!(
+                            "{} {} {} {} that {}",
+                            hash_action, subject, result, hash_result, relation
+                        );
                         // e.g. if the string *doesn't* start with SHA256
                         if prefix.chars().next().unwrap().is_lowercase() {
                             prefixes.push(initial_cap(&prefix));
@@ -63,11 +86,13 @@ fn gen_prefixes () -> Vec<String> {
                 }
             }
         }
-    }  
+    }
     return prefixes;
 }
 
-fn maybe_print_description_match (description: &str, matcher: &Regex) {
+// If there's a match, print it
+
+fn maybe_print_description_match(description: &str, matcher: &Regex) {
     let hash = digest(description.clone());
     //println!("{} {}", description, hash);
     if matcher.is_match(&hash) {
@@ -75,14 +100,19 @@ fn maybe_print_description_match (description: &str, matcher: &Regex) {
     }
 }
 
-fn maybe_find_feature (feature: &str, matcher: &Regex, prefixes: &Vec<String>) {
+// Look for the described feature, using the provided matcher, fuzzing with prefixes
+
+fn maybe_find_feature(feature: &str, matcher: &Regex, prefixes: &Vec<String>) {
     for prefix in prefixes {
         let description = format!("{} {}", prefix, feature);
-        maybe_print_description_match (&description, matcher);
+        maybe_print_description_match(&description, matcher);
     }
 }
 
-fn maybe_find_feature_count (feature: &str, matcher: &Regex, count: usize, prefixes: &Vec<String>) {
+// Look for the described feature, using the provided matcher,
+// matching AT LEAST count times, fuzzing with prefixes
+
+fn maybe_find_feature_count(feature: &str, matcher: &Regex, count: usize, prefixes: &Vec<String>) {
     for prefix in prefixes {
         let description = format!("{} {}", prefix, feature);
         let hash = digest(description.clone());
@@ -93,53 +123,86 @@ fn maybe_find_feature_count (feature: &str, matcher: &Regex, count: usize, prefi
     }
 }
 
-fn maybe_find_more_than_one (prefixes: &Vec<String>) {
+// Look for 0..F, matching > 1 times, fuzzing with prefixes
+
+fn maybe_find_more_than_one(prefixes: &Arc<Vec<String>>) {
     for digit in 0..15 {
-        let matcher = Regex::new(format!(r"{:x}{{2,}}", digit).as_str()).unwrap();
-        for qualifier in ["more than one", "multiple"] {
-            maybe_find_feature(&format!("{} {:x}", qualifier, digit), &matcher, prefixes);
-            // Use uppercase hex digits as well.
-            if digit > 9 {
-                maybe_find_feature(&format!("{} {:X}", qualifier, digit), &matcher, prefixes);
+        let p = prefixes.clone();
+        thread::spawn(move || {
+            let matcher = Regex::new(format!(r"{:x}{{2,}}", digit).as_str()).unwrap();
+            for qualifier in ["more than one", "multiple"] {
+                maybe_find_feature(&format!("{} {:x}", qualifier, digit), &matcher, &p);
+                // Use uppercase hex digits as well.
+                if digit > 9 {
+                    maybe_find_feature(&format!("{} {:X}", qualifier, digit), &matcher, &p);
+                }
             }
-        }
+        });
     }
 }
 
-fn maybe_find_n (prefixes: &Vec<String>) {
+// Look for 0..F, matching 5..15 times, fuzzing with prefixes
+
+fn maybe_find_n(prefixes: &Arc<Vec<String>>) {
     for digit in 0..15 {
         for count in 5..15 {
-            let matcher = Regex::new(format!(r"{:x}", digit).as_str()).unwrap();
-            maybe_find_feature_count(&format!("{} {:x}s", count, digit), &matcher, count, prefixes);
-            // Use uppercase hex digits as well.
-            if digit > 9 {
-                maybe_find_feature_count(&format!("{} {:X}s", count, digit), &matcher, count, prefixes);
-            }
+            let p = prefixes.clone();
+            thread::spawn(move || {
+                let matcher = Regex::new(format!(r"{:x}", digit).as_str()).unwrap();
+                maybe_find_feature_count(&format!("{} {:x}s", count, digit), &matcher, count, &p);
+                // Use uppercase hex digits as well.
+                if digit > 9 {
+                    maybe_find_feature_count(
+                        &format!("{} {:X}s", count, digit),
+                        &matcher,
+                        count,
+                        &p,
+                    );
+                }
+            });
         }
     }
 }
 
-fn maybe_find_n_successive (prefixes: &Vec<String>) {
+// Look for 0..F, matching 5.15 times SUCCESSIVELY, fuzzing with prefixes
+
+fn maybe_find_n_successive(prefixes: &Arc<Vec<String>>) {
     for n in 0..15 {
         for m in 5..15 {
-            let matcher = Regex::new(format!(r"{:x}{{{},}}", n, m).as_str()).unwrap();
-            maybe_find_feature(&format!("{} successive {:x}s", m, n), &matcher, prefixes);
-            maybe_find_feature(&format!("{} successive {:X}s", m, n), &matcher, prefixes);
-            maybe_find_feature(&format!("{} {:x}s successively", m, n), &matcher, prefixes);
-            maybe_find_feature(&format!("{} {:X}s successively", m, n), &matcher, prefixes);
+            let p = prefixes.clone();
+            thread::spawn(move || {
+                let matcher = Regex::new(format!(r"{:x}{{{},}}", n, m).as_str()).unwrap();
+                maybe_find_feature(&format!("{} successive {:x}s", m, n), &matcher, &p);
+                maybe_find_feature(&format!("{} successive {:X}s", m, n), &matcher, &p);
+                maybe_find_feature(&format!("{} {:x}s successively", m, n), &matcher, &p);
+                maybe_find_feature(&format!("{} {:X}s successively", m, n), &matcher, &p);
+            });
         }
     }
 }
 
+// Look for two different runs of 0..F, matching 5.15 times SUCCESSIVELY, fuzzing with prefixes
 // FIXME: describe and/or find exact matches
-fn maybe_find_n_and_m_successive (prefixes: &Vec<String>) {
-    for digit1 in 0..15 {     
+
+fn maybe_find_n_and_m_successive(prefixes: &Arc<Vec<String>>) {
+    for digit1 in 0..15 {
         for digit2 in 0..15 {
             if digit1 != digit2 {
-                for count1 in 3..15 {  
+                for count1 in 3..15 {
                     for count2 in 3..15 {
-                        let matcher = Regex::new(format!(r"{:x}{{{}}}.*{:x}{{{}}}", digit1, count1, digit2, count2).as_str()).unwrap();
-                        maybe_find_feature(&format!("{} {:x}s and {} {:x}s", count1, digit1, count2, digit2), &matcher, prefixes);
+                        let p = prefixes.clone();
+                        thread::spawn(move || {
+                            let matcher = Regex::new(
+                                format!(r"{:x}{{{}}}.*{:x}{{{}}}", digit1, count1, digit2, count2)
+                                    .as_str(),
+                            )
+                            .unwrap();
+                            maybe_find_feature(
+                                &format!("{} {:x}s and {} {:x}s", count1, digit1, count2, digit2),
+                                &matcher,
+                                &p,
+                            );
+                        });
                     }
                 }
             }
@@ -148,7 +211,7 @@ fn maybe_find_n_and_m_successive (prefixes: &Vec<String>) {
 }
 
 fn main() {
-    let prefixes = gen_prefixes();
+    let prefixes = Arc::new(gen_prefixes());
     maybe_find_more_than_one(&prefixes);
     maybe_find_n(&prefixes);
     maybe_find_n_successive(&prefixes);
